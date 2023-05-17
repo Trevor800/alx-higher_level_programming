3a98e8454f1c578f5b0d4047594277f141725c0e
Trevor800 <jeynator780@gmail.com>
2023-05-17 11:50:20 -0700

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    number = 0
-    for element in set(my_list):
-        number += element
-        return 
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num) 
