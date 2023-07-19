6dcc8942d68d52060aa214a10076a8f9cfb96e21
Trevor800 <jeynator780@gmail.com>
2023-07-19 13:57:48 -0700

    initial commit

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,3 @@
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS your_database_name;
-
--- Use the database
-USE your_database_name;
-
 -- Create the table 'id_not_null'
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT NOT NULL DEFAULT 1,
