ffdf1a5abec956024e3ef229dcc4ef3e854cc079
Trevor800 <jeynator780@gmail.com>
2023-07-19 14:01:21 -0700

    initial commit

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,9 +1,3 @@
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS your_database_name;
-
--- Use the database
-USE your_database_name;
-
 -- Create the table 'unique_id'
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1,
