4ffe8aebb9cf3d831bb0856c4922225222a77212
Trevor800 <jeynator780@gmail.com>
2023-07-19 14:25:13 -0700

    initial commit

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,9 @@
--- Use the database 'hbtn_0d_tvshows'
-USE hbtn_0d_tvshows;
-
--- List all genres of the show 'Dexter'
-SELECT tv_genres.name
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- uses a databse to lists all rows in a table corresponding to all rows in another
+SELECT name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-GROUP BY tv_genres.name
-ORDER BY tv_genres.name ASC;
+GROUP BY name
+ORDER BY name ASC;
