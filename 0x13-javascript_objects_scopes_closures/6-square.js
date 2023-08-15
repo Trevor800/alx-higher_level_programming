b24663780c9bc56d119ced1e6bb0c9d85157d6e7
Trevor800 <jeynator780@gmail.com>
2023-08-15 02:48:17 -0700

    my commits

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
-const SquareA = require('./5-rectangle.js');
-module.exports = class Square extends SquareA {
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
-      return this.print();
-    } else {
-      let A = '';
-      for (let i = 0; i < this.width; i++) {
-        A += c;
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
       }
-      for (let i = 0; i < this.height; i++) {
-        console.log(A);
-      }
+      console.log(s);
     }
   }
-};
+}
+
+module.exports = Square;
