14486bdf39a0250b0932a6ef51e1211bf17a1473
Trevor800 <jeynator780@gmail.com>
2023-08-20 08:12:28 -0700

    my commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
-# Define the database connection URL
-DATABASE_URI = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
-
-# Create an SQLAlchemy engine
-engine = create_engine(DATABASE_URI)
-
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Define a base class for declarative models
-Base = declarative_base()
-
-# Define the State model
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    # Query the states
-    states = session.query(State).filter(State.name == argv[4]).all()
-
-    # Print the results
-    for state in states:
-        print(state.id, state.name)
-
-    # Close the session
-    session.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
